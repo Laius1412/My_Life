ea44d1ac30c551e2e95625ca0cdd3be0b998d73c
Laius1412 <22010338@st.phenikaa-uni.edu.vn>
2025-01-09 17:19:36 +0700

    Kernel

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class RedirectIfAuthenticated extends Middleware
+{
+    // Logic to handle redirection if authenticated
+}
