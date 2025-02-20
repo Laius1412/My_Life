47603be30082b21d290aac8b061c995068fe6438
Laius1412 <118721682+Laius1412@users.noreply.github.com>
2025-02-20 15:58:47 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="box">
     <div class="img-cover">
-        <img src="{{ asset('img/avatar.jpg') }}" alt="Ava" class="img-ava">
+        <img src="{{ asset('img/logopage.png') }}" alt="Ava" class="img-ava">
     </div>
     @if (Auth::check())
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -18,19 +18,19 @@
         <a href="{{ route('login') }}" class="social">Login</a>
         <a href="{{ route('register') }}" class="social">Register</a>
     @endif
-    <a href="https://www.facebook.com/laius1108" target="_blank" class="social">
+    <a href="" target="_blank" class="social">
         <i class="fab fa-facebook-f"></i> Facebook
     </a>
-    <a href="https://www.instagram.com/laius4teen/" target="_blank" class="social">
+    <a href="" target="_blank" class="social">
         <i class="fab fa-instagram"></i> Instagram
     </a>
-    <a href="https://mail.google.com/mail/u/0/#inbox" class="social">
+    <a href="" class="social">
         <i class="fas fa-envelope"></i> Email
     </a>
-    <a href="https://zalo.me" target="_blank" class="social">
+    <a href="" target="_blank" class="social">
         <i class="fas fa-comments"></i> Zalo
     </a>
-    <a href="https://github.com/Laius1412" target="_blank" class="social">
+    <a href="" target="_blank" class="social">
         <i class="fab fa-github"></i> Github
     </a>
 </div>
